dbe1b49f0e0c54540ac96dd00f4e7220c5794a40
Steven Heinrich <steven.heinrich@gmail.com>
2013-01-08 17:03:06 -0500

    Update jquery.log.js
    Add default message text
    Fix indentation

--- jquery.log.js
+++ jquery.log.js
@@ -1,11 +1,11 @@
 /**
  * log plugin for jQuery
  * v1.0
- * executes console.log with you message
+ * Executes console.log with your optional message
  * to show what you are currently accessing.
  * (known to work in Firefox and Chrome)
  *
- * Plugin authored by Steve Heinrich, to encapsulate
+ * Plugin authored by Steven Heinrich, to encapsulate
  * the function and concept from Dominic Mitchell
  * http://happygiraffe.net/blog/2007/09/26/jquery-logging
  *
@@ -22,8 +22,9 @@
 
 (function($) {
   $.fn.log = function (msg) {
-	  if (!window.console || !console) return;
-      console.log("%s: %o", msg, this);
-      return this;
-  	};
+    if (!window.console || !console) return;
+    if (!msg) msg = 'console.log';
+    console.log("%s: %o", msg, this);
+    return this;
+  };
 })(jQuery);
